0350d8dc1d14872a3fc85b5a33c476c0db951c2a
bruno-jose-araujo <bruno.jose.araujo.99@gmail.com>
2025-03-26 12:37:32 +0000

    Initial
    Simply added Dockerfile and an autobuild.sh script to automate building, compiling, copying binaries to host and testing.

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# Use a base image with the required environment (e.g., Ubuntu, Arch, or Debian)
+FROM archlinux:latest
+
+# Install dependencies
+RUN pacman -Syu --noconfirm base-devel \
+    gcc \
+    meson \
+    ninja \
+    glslang \
+    mesa \
+    libx11 \
+    xcb-util \
+    python-mako \
+    nvidia-utils \
+    dbus \
+    wayland \
+    cmake \
+    pkgconf \
+    libxkbcommon \
+    git \
+    libxnvctrl \
+    appstream
+
+# Set the working directory inside the container
+WORKDIR /workspace
+
+# Copy the source code of MangoHUD into the container (this assumes you're building from source)
+COPY . /workspace
+
+# Set the default command to start bash or any other command you need
+CMD ["bash"]
---
+++ autobuild.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# Automation of building, compiling, copying to host, and testing of current Mangohud source code.
+# To be ran in host bash from project root, NOT by Docker container.
+
+echo "Enter Docker container name/id:"
+read container_name
+
+
+if [ -d "build" ]; then
+    rm -r build
+fi
+#echo -e "\e[►Previous build config cleaned\e[0m"
+
+docker exec -it "$container_name" bash -c "meson build"
+#echo -e "\e[32m►Build complete\e[0m"
+docker exec -it "$container_name" bash -c "ninja -C build install"
+#echo -e "\e[32m►Compile successfull\e[0m"
+
+if [ -f "/usr/local/bin/mangohud" ]; then
+    sudo rm /usr/local/bin/mangohud
+fi
+
+if [ -f "/usr/local/bin/mangoplot" ]; then
+    sudo rm /usr/local/bin/mangoplot
+fi
+
+sudo pacman -R --noconfirm goverlay mangohud
+#echo -e "\e[32m►Previous MangoHUD binaries cleaned form host\e[0m"
+
+docker cp "$container_name:/usr/local/bin/mangohud" /home/brunoaraujo/Desktop/
+docker cp "$container_name:/usr/local/bin/mangoplot" /home/brunoaraujo/Desktop/
+
+sudo mv /home/brunoaraujo/Desktop/mangohud /usr/local/bin/
+sudo mv /home/brunoaraujo/Desktop/mangoplot /usr/local/bin/
+#echo -e "\e[32m►MangoHUD binaries copied to host\e[0m"
+
+sudo pacman -S --noconfirm goverlay
+
+goverlay
+#echo -e "\e[32m►Done\e[0m"
